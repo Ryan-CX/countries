e2fa8c3dccbf351a5e860c58e5721182cd2bc8fa
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-04-13 21:01:37 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,105 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import List from './List';
+const style = {
+	margin: '20px',
+};
 
-function App() {
-	const [input, setInput] = useState('');
+const flagStyle = {
+	width: '180px',
+};
 
-	const handleInput = (e) => {
-		setInput(e.target.value);
+const ShowCountry = ({ country }) => {
+	const [weatherDetails, setWeatherDetails] = useState({});
+
+	const { name, capital, population, languages, flag } = country;
+	const api_key = process.env.api_key;
+
+	useEffect(() => {
+		axios
+			.get(
+				`http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`
+			)
+			.then((response) => {
+				setWeatherDetails(response.data.current);
+			});
+		return () => setWeatherDetails({});
+	}, [api_key, capital]);
+
+	return (
+		<div>
+			<h1>{name}</h1>
+			<p>capital {capital}</p>
+			<p>population {population}</p>
+			<h2>Spoken Languages</h2>
+			<ul>
+				{languages.map((language) => (
+					<li key={language.name}>{language.name}</li>
+				))}
+			</ul>
+			<img src={flag} alt='flag' style={flagStyle}></img>
+			<h2>Weather at {capital}</h2>
+			<p>
+				<b>temperature</b>: {weatherDetails.temperature} Celcius
+			</p>
+			<img src={weatherDetails.weather_icons} alt='weather icon'></img>
+			<p>
+				<b>wind:</b> {weatherDetails.wind_speed} mph direction{' '}
+				{weatherDetails.wind_dir}
+			</p>
+		</div>
+	);
+};
+
+const ListCountries = ({ query }) => {
+	const [countries, setCountries] = useState([]);
+	const [country, setCountry] = useState({});
+
+	useEffect(() => {
+		if (query !== '') {
+			axios
+				.get(`https://restcountries.eu/rest/v2/name/${query}`)
+				.then((response) => {
+					setCountries(response.data);
+				});
+		}
+	});
+
+	const handleShow = (country) => {
+		setCountry(country);
 	};
 
 	return (
 		<div>
-			find countries
-			<input value={input} onChange={handleInput} />
-			<List query={input} />
+			{countries.length > 10 && <p>too many matches, specify another filter</p>}
+			{countries.length < 10 &&
+				countries.length !== 1 &&
+				countries.map((country) => (
+					<div key={country.name}>
+						{country.name}
+						<button onClick={() => handleShow(country)}>show</button>
+					</div>
+				))}
+			{country.languages !== undefined && <ShowCountry country={country} />}
 		</div>
 	);
-}
+};
 
+const App = () => {
+	const [query, setQuery] = useState('');
+
+	const handleChange = (event) => {
+		event.preventDefault();
+		setQuery(event.target.value);
+	};
+
+	return (
+		<div style={style}>
+			<div>
+				find countries <input onChange={handleChange} value={query}></input>
+			</div>
+			<ListCountries query={query} />
+		</div>
+	);
+};
 export default App;
